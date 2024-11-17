7316bdcfa0c9c754db6c6bf4b768fb0ba4e89054
LaRoca33 <arocaangel19@gmail.com>
2024-11-17 15:37:55 +0100

    Commit dia17-011, botones con sus respectivas funciones e internacionalización completados

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -4,3 +4,51 @@ document.getElementById("locales").addEventListener("change", function () {
         window.location.href = window.location.pathname + "?lang=" + selectedLang;
     }
 });
+
+function botonClave (){
+
+    let claveInput = document.getElementById('clave');
+    let confirmarClaveInput = document.getElementById('confirmarClave');
+    if (claveInput.type === 'password') {
+        claveInput.type = 'text';
+        confirmarClaveInput.type = 'text';
+    } else {
+        claveInput.type = 'password';
+        confirmarClaveInput.type = 'password';
+    }
+}
+
+// Función para deseleccionar géneros
+// Función para deseleccionar géneros
+function botonGeneros() {
+    let generoRadio = document.getElementsByName('generoSeleccionado');
+    for (let i = 0; i < generoRadio.length; i++) {
+        generoRadio[i].checked = false;
+    }
+}
+
+function botonMusicasDes() {
+    let selMusica = document.getElementById('musicasSeleccionadas');
+    for (let i = 0; i < selMusica.options.length; i++) {
+        selMusica.options[i].selected = false;
+    }
+}
+
+function botonMusicasSel() {
+    let selMusica = document.getElementById('musicasSeleccionadas');
+    for (let i = 0; i < selMusica.options.length; i++) {
+        selMusica.options[i].selected = true;
+    }
+}
+
+function botonAficionesDes(){
+    for (let i=0; i< document.getElementsByName('aficionesSeleccionadas').length;i++){
+        document.getElementsByName('aficionesSeleccionadas')[i].checked=false;
+    }
+}
+
+function botonAficionesSel(){
+    for (let i=0; i< document.getElementsByName('aficionesSeleccionadas').length;i++){
+        document.getElementsByName('aficionesSeleccionadas')[i].checked=true;
+    }
+}
